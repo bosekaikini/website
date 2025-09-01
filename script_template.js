2018f50db89f5e20aa19cf486913557632131da4
Bose Kaikini <bose.kaikini@gmail.com>
2025-09-01 19:37:52 -0400

    Create script_template.js

---
+++ script_template.js
@@ -0,0 +1,40 @@
+function closePane() {
+    document.getElementById("contactPane").classList.remove("active");
+}
+
+const projGoButton = document.getElementById('project_button');
+const homeGoButton = document.getElementById('home_button');
+const blogGoButton = document.getElementById('blog_button');
+const resumeGoButton = document.getElementById('resume_button');
+const courseworkGoButton = document.getElementById('coursework_button');
+const contactButton = document.getElementById("contact_button");
+const contactPane = document.getElementById("contactPane");
+
+projGoButton.addEventListener('click', () => {
+    window.location.href = 'project_driver.html';
+});
+
+homeGoButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+blogGoButton.addEventListener('click', () => {
+    window.location.href = 'blog_driver.html';
+});
+
+resumeGoButton.addEventListener('click', () => {
+    // Rename file to remove space in filename for safety
+    window.open('external_files/Bose_Kaikini_Resume_2025_July.pdf');
+});
+
+courseworkGoButton.addEventListener('click', () => {
+    window.open('external_files/Bose_Kaikini_Courses_2025.pdf');
+});
+
+contactButton.addEventListener('click', () => {
+    contactPane.classList.add('active');
+});
+
+function openContactPane() {
+    contactPane.classList.add('active');
+}
