324638b17a848bc69cd3cb732976d6c2e0b35807
Bose Kaikini <bose.kaikini@gmail.com>
2025-09-01 19:07:35 -0400

    Update script_template.js

--- template_files/script_template.js
+++ template_files/script_template.js
@@ -10,23 +10,23 @@ const contactButton=document.getElementById("contact_button");
 const contactPane=document.getElementById("contactPane");
 
 projGoButton.addEventListener('click', function() {
-    window.location.href='../project_driver.html';
+    window.location.href='project_driver.html';
 });
 
 homeGoButton.addEventListener('click', function() {
-    window.location.href='../index.html';
+    window.location.href='index.html';
 });
 
 blogGoButton.addEventListener('click', function() {
-    window.location.href='../blog_driver.html';
+    window.location.href='blog_driver.html';
 });
 
 resumeGoButton.addEventListener('click', function() {
-    window.open('../external_files/Bose Kaikini_Resume_2025_July.pdf');
+    window.open('external_files/Bose Kaikini_Resume_2025_July.pdf');
 });
 
 courseworkGoButton.addEventListener('click', function() {
-    window.open('../external_files/Bose_Kaikini_Courses_2025.pdf');
+    window.open('external_files/Bose_Kaikini_Courses_2025.pdf');
 });
 
 contactButton.addEventListener('click', function() {
