33dff5c19cc3027d729715d18ac5f63ba5f75312
Bose Kaikini <bose.kaikini@gmail.com>
2025-09-02 10:40:41 -0400

    Update script_template.js

--- js/script_template.js
+++ js/script_template.js
@@ -1,6 +1,3 @@
-function closePane() {
-    document.getElementById("contactPane").classList.remove("active");
-}
 const projGoButton=document.getElementById('project_button');
 const homeGoButton=document.getElementById('home_button');
 const blogGoButton=document.getElementById('blog_button');
@@ -8,6 +5,7 @@ const resumeGoButton=document.getElementById('resume_button');
 const courseworkGoButton=document.getElementById('coursework_button');
 const contactButton=document.getElementById("contact_button");
 const contactPane=document.getElementById("contactPane");
+const closeButton=document.getElementById("close_button");
 
 projGoButton.addEventListener('click', function() {
     window.location.href='project_driver.html';
@@ -33,7 +31,7 @@ contactButton.addEventListener('click', function() {
     contactPane.classList.add('active');
 });
 
-function openContactPane() {
-    contactPane.classList.add('active');
-}
+closeButton.addEventListener('click', function() {
+    contactPane.classList.remove("active");
+});
 
